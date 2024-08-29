b0fac2e0f02e95841ea2b7bb9b2ab16d307283e6
Kevklatman <116696816+Kevklatman@users.noreply.github.com>
2024-08-29 08:39:40 -0600

    tree, bush, and

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,5 +1,6 @@
 def oxford_comma(items):
     items.insert(-1, 'and')
-    ', '.join(items)
-    return items
+    new_items= ', '.join(items[:-1])
+    
+    return new_items
 print(oxford_comma(['tree', 'bush', 'house']))
